8b3afd727a8e2442fe11059b276a013a85615ebd
MaxCalhoun <maxpatcal@outlook.com>
2020-12-02 12:43:21 -0500

    Added test cases to verify different properties of constructor

--- Video_t.cpp
+++ Video_t.cpp
@@ -13,12 +13,12 @@ TEST_CASE( "NOP", "[Video]" ) {
 
 }
 
-TEST_CASE("Original Video", "[Video]")
+TEST_CASE("Empty Original Video", "[Video]")
 {
-	const auto title = "The Original Movie";
+	const auto title = "";
 	const auto type = Video::ORIGINAL;
-	const auto numHours = 2;
-	const auto numMinutes = 7;
+	const auto numHours = 0;
+	const auto numMinutes = 0;
 	const auto numEpisodes = 0;
 
 	auto video = Video(title, type, numHours, numMinutes, numEpisodes);
@@ -30,3 +30,58 @@ TEST_CASE("Original Video", "[Video]")
 	REQUIRE(video.Minutes() == numMinutes);
 	REQUIRE(video.episodes() == numEpisodes);
 }
+
+TEST_CASE("Original Video with Title", "[Video]")
+{
+        const auto title = "The Original Movie 2";
+        const auto type = Video::ORIGINAL;
+        const auto numHours = 0;
+        const auto numMinutes = 0;
+        const auto numEpisodes = 0;
+
+        auto video = Video(title, type, numHours, numMinutes, numEpisodes);
+
+        // Check that member variables are correct
+        REQUIRE(video.title() == title);
+        REQUIRE(video.type() == type);
+        REQUIRE(video.hours() == numHours);
+        REQUIRE(video.Minutes() == numMinutes);
+        REQUIRE(video.episodes() == numEpisodes);
+}
+
+TEST_CASE("Original Video with Time", "[Video]")
+{
+        const auto title = "The Unoriginal Documentary";
+        const auto type = Video::ORIGINAL;
+        const auto numHours = 1;
+        const auto numMinutes = 23;
+        const auto numEpisodes = 0;
+
+        auto video = Video(title, type, numHours, numMinutes, numEpisodes);
+
+        // Check that member variables are correct
+        REQUIRE(video.title() == title);
+        REQUIRE(video.type() == type);
+        REQUIRE(video.hours() == numHours);
+        REQUIRE(video.Minutes() == numMinutes);
+        REQUIRE(video.episodes() == numEpisodes);
+}
+
+TEST_CASE("Original Video with Episodes", "[Video]")
+{
+        const auto title = "Cool Original Show";
+        const auto type = Video::ORIGINAL;
+        const auto numHours = 1;
+        const auto numMinutes = 20;
+        const auto numEpisodes = 10;
+
+        auto video = Video(title, type, numHours, numMinutes, numEpisodes);
+
+        // Check that member variables are correct
+        REQUIRE(video.title() == title);
+        REQUIRE(video.type() == type);
+        REQUIRE(video.hours() == numHours);
+        REQUIRE(video.Minutes() == numMinutes);
+        REQUIRE(video.episodes() == numEpisodes);
+}
+
