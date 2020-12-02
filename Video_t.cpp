ec6862cee64d35afcf6f25ca9534f318d14f551d
MaxCalhoun <maxpatcal@outlook.com>
2020-12-01 21:07:32 -0500

    Created test case for making an Original Video [issue 1]

--- Video_t.cpp
+++ Video_t.cpp
@@ -12,3 +12,21 @@
 TEST_CASE( "NOP", "[Video]" ) {
 
 }
+
+TEST_CASE("Original Video", "[Video]")
+{
+	const auto title = "The Original Movie";
+	const auto type = Video::ORIGINAL;
+	const auto numHours = 2;
+	const auto numMinutes = 7;
+	const auto numEpisodes = 0;
+
+	auto video = Video(title, type, numHours, numMinutes, numEpisodes);
+
+	// Check that member variables are correct
+	REQUIRE(video.title() == title);
+	REQUIRE(video.type() == type);
+	REQUIRE(video.hours() == numHours);
+	REQUIRE(video.Minutes() == numMinutes);
+	REQUIRE(video.episodes() == numEpisodes);
+}
