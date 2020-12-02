d0d3713c1deef1edbc826fdc951ed6bb873538f8
Sherlin <msherlin@wasp.cs.kent.edu>
2020-12-01 20:42:14 -0500

    Create Account.cpp single stream test

--- Account_t.cpp
+++ Account_t.cpp
@@ -16,3 +16,16 @@ TEST_CASE( "No streaming", "[Account]" ) {
     REQUIRE(customer.name() == "Fred");
     REQUIRE(customer.data() == "");
 }
+
+//Test case single stream
+TEST_CASE( "addStream() w/ single stream", "[Account]"){
+
+  auto episodes= 0;
+  auto video= Video("Billy Madison", Video::MOVIE, 1, 45, episodes);
+  auto stream= Stream(video, 1);
+  auto customer= Account("Matt");
+
+  customer.addStream(stream);
+
+  REQUIRE(customer.data() == "Matt,MOVIE,Billy Madison,1,45,1\n");
+}
