3c24f1bdb7b12bfc1603f65b8d2c8fbfc104c152
Odenthal <jodentha@wasp.cs.kent.edu>
2020-12-14 20:02:45 -0500

    Extract and move method for Account::report() completed

--- Account_t.cpp
+++ Account_t.cpp
@@ -16,3 +16,64 @@ TEST_CASE( "No streaming", "[Account]" ) {
     REQUIRE(customer.name() == "Fred");
     REQUIRE(customer.data() == "");
 }
+//Test case single stream MOVIE
+TEST_CASE( "addStream() w/ single stream MOVIE", "[Account]"){
+
+  auto episodes= 0;
+  auto video= Video("Billy Madison", Video::MOVIE, 1, 45, episodes);
+  auto stream= Stream(video, 1);
+  auto customer= Account("Matt");
+
+  customer.addStream(stream);
+
+  REQUIRE(customer.data() == "Matt,MOVIE,Billy Madison,1,45,1\n");
+}
+
+//Test case single stream TVSHOW 
+TEST_CASE( "addStream() w/ single stream TVSHOW", "[Account]"){
+
+  auto episodes= 12;
+  auto video= Video("America's Next Top Model", Video::TVSHOW, 0, 45, episodes);
+  auto stream= Stream(video, 40);
+  auto customer= Account("Dylan");
+
+  customer.addStream(stream);
+
+  REQUIRE(customer.data() == "Dylan,TVSHOW,America's Next Top Model,0,1800,40\n");
+}
+
+//Test case single stream ORIGINAL
+TEST_CASE( "addStream() w/ single stream ORIGINAL", "[Account]"){
+
+  auto episodes= 4;
+  auto video= Video("Wonder Bread the Documentary", Video::ORIGINAL, 0, 20, episodes);
+  auto stream= Stream(video, 3);
+  auto customer= Account("Alva");
+
+  customer.addStream(stream);
+
+  REQUIRE(customer.data() == "Alva,ORIGINAL,Wonder Bread the Documentary,0,60,3\n");
+}
+
+//Test case stream report
+TEST_CASE( "report() w/ 2 streams", "[Account]"){
+
+  auto episodes= 4;
+  auto video= Video("Wonder Bread the Documentary", Video::ORIGINAL, 0, 20, episodes);
+  auto stream= Stream(video, 1);
+  auto customer= Account("Alva");
+
+  customer.addStream(stream);
+
+  auto episodes2= 0;
+  auto video2= Video("Iron Man", Video::MOVIE, 1, 15, episodes2);
+  auto stream2= Stream(video, 14);
+
+  customer.addStream(stream2);
+
+  customer.report();
+  
+  REQUIRE(stream.viewings() == 1);
+  REQUIRE(stream2.viewings() == 14);
+
+}
--- Video.hpp
+++ Video.hpp
@@ -1,6 +1,6 @@
 /*
   Video.hpp
-
+0;136;0c
   Include file for Video class
 */
 
@@ -8,7 +8,6 @@
 #define VIDEO_H
 
 #include <string>
-
 class Video {
 public:
 
@@ -36,6 +35,9 @@ public:
 
     // set video length
     void setLength(int hours, int minutes);
+
+  int getNumberOfOriginals();
+  int getNumberOfStreams(const int);
 
 private:
     std::string videoTitle;
